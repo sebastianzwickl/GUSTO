8b0dd62231678cc76ac2501baf847cde106cb97f
Sebastian Zwickl-Bernhard <zwickl@eeg.tuwien.ac.at>
2020-08-03 17:46:12 +0200

    Fix typos in write_globvar_to_paretofront()

--- write_globalvalues_to_pandas.py
+++ write_globalvalues_to_pandas.py
@@ -17,7 +17,8 @@ def write_globvar_to_paretofront():
     os.chdir(wd)
     name_files = os.listdir()
     for n in reversed(range(len(name_files))):
-        if ('scenario_' not in name_files[n] and '.xlsx' not in name_files[n] and
+        if ('scenario_' not in name_files[n] and
+                '.xlsx' not in name_files[n] and
                 '~' in name_files[n]):
             name_files.pop(n)
     del[n, wd, e]
